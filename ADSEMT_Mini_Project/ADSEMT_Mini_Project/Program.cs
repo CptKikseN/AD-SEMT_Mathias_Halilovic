35b63ee5b849dd8f4781ce8b637051f1fb922e3d
Mathias Halilovic <mathias.halilovic94@gmail.com>
2021-04-26 18:46:41 +0200

    Script ready

---
+++ ADSEMT_Mini_Project/ADSEMT_Mini_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADSEMT_Mini_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
